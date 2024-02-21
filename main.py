59857f463dee8ec2d127d2a6862fe582ec0779c6
AkbarYusupov2003 <akbarbek.yusupov@gmail.com>
2024-02-21 15:52:22 +0500

    update

---
+++ main.py
@@ -0,0 +1,12 @@
+import nemo.collections.asr as nemo_asr
+
+
+# Загрузка предварительно обученной модели диаризации говорящих
+model = nemo_asr.models.SpeakerDiarizer.from_pretrained(model_name="titanet_large")
+
+# Путь к вашему аудиофайлу
+audio_filepath = "data/le.wav"
+
+# Применение модели к аудиофайлу
+# Это сгенерирует аннотации диаризации в формате RTTM
+model.diarize(paths2audio_files=[audio_filepath])
