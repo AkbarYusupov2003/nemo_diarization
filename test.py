f312821c009d5f78b694e06441875dbadf4afac9
Akbar Yusupov <akbarbek.yusupov@gmail.com>
2024-03-05 18:23:29 +0500

    update

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained("nvidia/speakerverification_en_titanet_large")
 model.to(device)
+model.eval()
 
 audio_path = "/home/user/Desktop/projects/nemo_diarization/data/new.wav"
 audio_length = 30.306644
@@ -32,6 +33,25 @@ if len(audio_signal.shape) == 1:
     audio_signal = audio_signal.unsqueeze(0)
 
 signal_length = torch.tensor([audio_signal.shape[1]])
+signal_length = signal_length.to(device)
 audio_signal = audio_signal.to(device)
 embeddings = model.forward(input_signal=audio_signal, input_signal_length=signal_length)
-print("embeddings", embeddings)
+
+# Assuming `embeddings` is your tensor of embeddings from the model
+embeddings_np = embeddings[1].detach().cpu().numpy()  # Convert embeddings to NumPy array
+
+print("++++++++++++++")
+print(embeddings_np.shape)
+print("++++++++++++++")
+
+# Perform K-Means clustering
+n_clusters = 3  # Adjust based on your audio
+kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(embeddings_np)
+
+# The labels assigned to each embedding
+speaker_labels = kmeans.labels_
+
+timestamps_and_speakers = [(i, speaker_labels[i]) for i in range(len(speaker_labels))]
+
+for timestamp, speaker in timestamps_and_speakers:
+    print(f"Time: {timestamp} - Speaker: {speaker}")
